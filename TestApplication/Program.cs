a4eace6c0eca630f2ae8eac99726ab33bb5c5971
lazarev_a <lazarev_a@auca.kg>
2024-09-03 18:17:40 +0600

    Docker final fix

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -5,11 +5,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -24,4 +22,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+app.Run("http://*:80"); // Ensure the application listens on port 80
